b0e2ec979ccf8c08509ebfc75fe4aed2f70ef7c0
itschip <chip@projecterror.dev>
2023-10-21 22:21:26 +0200

    feat: server pkg

---
+++ pkg/server/server.go
@@ -0,0 +1,62 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/itschip/guildedgo/pkg/client"
+)
+
+type Server struct {
+	ID         string `json:"id"`
+	OwnerID    string `json:"ownerid"`
+	Type       string `json:"type,omitempty"`
+	Name       string `json:"name"`
+	URL        string `json:"url,omitempty"`
+	About      string `json:"about,omitempty"`
+	Avatar     string `json:"avatar,omitempty"`
+	Banner     string `json:"banner,omitempty"`
+	Timezone   string `json:"timezone,omitempty"`
+	IsVerified bool   `json:"isVerified,omitempty"`
+
+	// The channel ID of the default channel of the server.
+	// This channel is defined as the first chat or voice channel in the left sidebar of a server in our UI.
+	// This channel is useful for sending welcome messages,
+	// though note that a bot may not have permissions to interact with this channel depending on how the server is configured.
+	DefaultChannelId string `json:"defaultChannelId,omitempty"`
+	CreatedAt        string `json:"createdAt"`
+}
+
+type serverResponse struct {
+	Server `json:"server"`
+}
+
+const (
+	TypeTeam         string = "team"
+	TypeOrganization string = "organization"
+	TypeCommunity    string = "community"
+	TypeClan         string = "clan"
+	TypeGuild        string = "guild"
+	TypeFriends      string = "friends"
+	TypeStreaming    string = "streaming"
+	TypeOther        string = "other"
+)
+
+func Get(c *client.Client, serverID string) (Server, error) {
+	var err error
+	endpoint := client.GuildedApi + "/servers/" + serverID
+
+	var v serverResponse
+
+	body, err := c.Http.PerformRequest(http.MethodGet, endpoint, nil)
+	if err != nil {
+		return Server{}, fmt.Errorf("failed to get server: %w", err)
+	}
+
+	err = c.Http.Decode(body, &v)
+	if err != nil {
+		return Server{}, fmt.Errorf("failed to decode server response: %w", err)
+	}
+
+	return v.Server, nil
+}
---
+++ pkg/server/server_test.go
@@ -0,0 +1,34 @@
+package server_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/itschip/guildedgo/pkg/client"
+	"github.com/itschip/guildedgo/pkg/server"
+	"github.com/joho/godotenv"
+)
+
+func TestGetServer(t *testing.T) {
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+
+	token := os.Getenv("TOKEN")
+	serverID := os.Getenv("SERVER_ID")
+
+	c := client.New(client.Config{
+		ServerID: serverID,
+		Token:    token,
+	})
+
+	s, err := server.Get(c, serverID)
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+
+	t.Log(s)
+}
