174b7f9fea0317aabe243d4b24d80e138cb2b6a3
itschip <chip@projecterror.dev>
2023-10-30 14:36:43 +0100

    feat: complete user pkg

--- pkg/user/user.go
+++ pkg/user/user.go
@@ -1,5 +1,12 @@
 package user
 
+import (
+	"fmt"
+	"github.com/itschip/guildedgo/pkg/client"
+	"github.com/itschip/guildedgo/pkg/server"
+	"net/http"
+)
+
 type User struct {
 	// The ID of the user
 	Id string `json:"id"`
@@ -26,6 +33,12 @@ type Status struct {
 	EmoteID int    `json:"emoteId"`
 }
 
+type UpdateStatusParams struct {
+	Content   string `json:"content,omitempty"`
+	EmoteID   int    `json:"emoteId"`
+	ExpiresAt string `json:"expiresAt,omitempty"`
+}
+
 type Summary struct {
 	// The ID of the user
 	Id string `json:"id"`
@@ -37,4 +50,72 @@ type Summary struct {
 
 	// The avatar image associated with the user
 	Avatar string `json:"avatar,omitempty"`
+}
+
+func Get(c *client.Client, userID string) (*User, error) {
+	var err error
+	endpoint := client.GuildedApi + "/users/" + userID
+
+	var v struct {
+		User User `json:"user"`
+	}
+
+	res, err := c.Http.PerformRequest(http.MethodGet, endpoint, nil)
+	if err != nil {
+		return nil, fmt.Errorf("error getting user: %w", err)
+	}
+
+	err = c.Http.Decode(res, &v)
+	if err != nil {
+		return nil, fmt.Errorf("error decoding user: %w", err)
+	}
+
+	return &v.User, nil
+}
+
+// GetServers returns a list of servers that the user is in.
+// Note - at this time, you can only retrieve your own servers
+func GetServers(c *client.Client, userID string) ([]server.Server, error) {
+	var err error
+	endpoint := client.GuildedApi + "/users/" + userID + "/servers"
+
+	var v struct {
+		Servers []server.Server `json:"servers"`
+	}
+
+	res, err := c.Http.PerformRequest(http.MethodGet, endpoint, nil)
+	if err != nil {
+		return nil, fmt.Errorf("error getting user servers: %w", err)
+	}
+
+	err = c.Http.Decode(res, &v)
+	if err != nil {
+		return nil, fmt.Errorf("error decoding user servers: %w", err)
+	}
+
+	return v.Servers, nil
+}
+
+func UpdateStatus(c *client.Client, userID string, params *UpdateStatusParams) error {
+	var err error
+	endpoint := client.GuildedApi + "/users/" + userID + "/status"
+
+	_, err = c.Http.PerformRequest(http.MethodPut, endpoint, params)
+	if err != nil {
+		return fmt.Errorf("error updating user status: %v", err)
+	}
+
+	return nil
+}
+
+func DeleteStatus(c *client.Client, userID string) error {
+	var err error
+	endpoint := client.GuildedApi + "/users/" + userID + "/status"
+
+	_, err = c.Http.PerformRequest(http.MethodDelete, endpoint, nil)
+	if err != nil {
+		return fmt.Errorf("error deleting user status: %v", err)
+	}
+
+	return nil
 }
